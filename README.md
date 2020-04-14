99822a15d53f0e78d233c3770a470aa3343e4d15
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-14 11:00:04 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# My Java Exercise 1" 
---
+++ src/MyJavaExercise1.java
@@ -0,0 +1,24 @@
+public class MyJavaExercise1 {
+    public static void main (String[] args)
+    {
+        boolean flag = true;
+        byte b = 4;
+        char a = 'A';
+        short index = 3;
+        int i = 89;
+        float f = 4.73f;
+        long longVar = 555;
+        double doubleVar = 123.123;
+
+        System.out.println("flag; " + flag);
+        System.out.println("byte: " + b);
+        System.out.println("char: " + a);
+        System.out.println("short: " + index);
+        System.out.println("integer: " + i);
+        System.out.println("float:" + f);
+        System.out.println("long: " + longVar);
+        System.out.println("double: " + doubleVar);
+
+
+    }
+}
